237d8ace0d74cf3486343b6fae37752550349003
Jawad Ahmed <jawad.ahmed91@live.com>
2020-01-18 19:19:49 +0500

    update

--- Blog.Service/CategoryServices/CategoryService.cs
+++ Blog.Service/CategoryServices/CategoryService.cs
@@ -11,27 +11,28 @@ namespace Blog.Service.CategoryServices
     public class CategoryService : ICategoryService
     {
         private BlogEntities DbContext;
-        
-        public CategoryService(BlogEntities _DbContext) 
+
+        public CategoryService(BlogEntities _DbContext)
         {
-            DbContext = _DbContext;        
+            DbContext = _DbContext;
         }
         public IEnumerable<CategoryViewModel> getAll()
         {
             IEnumerable<CategoryViewModel> list = new List<CategoryViewModel>();
-            using (DbContext) {
+            using (DbContext)
+            {
                 list = DbContext.Categories.ToList().Select(m => new CategoryViewModel
                 {
-                    CategoryId=m.Id,
-                    CategoryName=m.CategoryName,
-                    Description=m.Description,
-                    CreateBy=m.CreateBy,
-                    CreateDate=m.CreateDate,
-                    ModifyBy=m.ModifyBy,
-                    ModifyDate=m.ModifyDate,
-                    Active=m.Active,
-                    CSSStyle=m.CSSStyle,
-                    ImageUrl=m.ImageUrl
+                    CategoryId = m.Id,
+                    CategoryName = m.CategoryName,
+                    Description = m.Description,
+                    CreateBy = m.CreateBy,
+                    CreateDate = m.CreateDate,
+                    ModifyBy = m.ModifyBy,
+                    ModifyDate = m.ModifyDate,
+                    Active = m.Active,
+                    CSSStyle = m.CSSStyle,
+                    ImageUrl = m.ImageUrl
                 });
             }
             return list;
@@ -40,10 +41,10 @@ namespace Blog.Service.CategoryServices
         public CategoryViewModel getCategoryById()
         {
             using (DbContext)
-            { 
+            {
                 var model = DbContext.Categories.SingleOrDefault();
 
-                CategoryViewModel category= new CategoryViewModel
+                CategoryViewModel category = new CategoryViewModel
                 {
                     CategoryId = model.Id,
                     CategoryName = model.CategoryName,
@@ -58,7 +59,7 @@ namespace Blog.Service.CategoryServices
                 };
                 return category;
             }
-       
+
         }
     }
 }
